19252381abd4de1223168a28cf558ab22022928c
Christian Guenther <ch.gpunkt@gmail.com>
2025-01-22 11:27:40 +0100

    refactored to meet 3D conditions

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -41,8 +41,9 @@ struct KdTree
 	void insertHelper(Node*& node, uint depth, const std::vector<float>& point, int id) {
         if (node == NULL) {
             node = new Node(point, id);
+            return;
         } else {
-            uint splitAxis = depth % 2;
+            uint splitAxis = depth % 3;
             if (point[splitAxis] < node->point[splitAxis]) {
                 insertHelper(node->left, depth + 1, point, id);
             } else {
@@ -56,45 +57,79 @@ struct KdTree
 		insertHelper(root, 0, point, id);
 	}
 
-	// check if the node point is within a boxed square which is 2 times the tolerance
-    bool isInBox(std::vector<float> pointToCheck, std::vector<float> treePoint, float distanceTol)
-    {
-        if ((treePoint[0] < (pointToCheck[0] - distanceTol)) || (treePoint[0] > (pointToCheck[0] + distanceTol)) || (treePoint[1] < (pointToCheck[1] - distanceTol)) || (treePoint[1] > (pointToCheck[1] + distanceTol))) {
-            return false;
-        }
-        return true;
-    }
 
-    // calculate if the distance between node point and point to check point is within tolerance
-    bool isInDistanceTol(std::vector<float> pointToCheck, std::vector<float> treePoint, float distanceTol)
-    {
-        float d = sqrtf(pow((pointToCheck[0] - treePoint[0]), 2.0) + pow((pointToCheck[1] - treePoint[1]), 2.0));
-
-        if (d < distanceTol) {
-            return true;
-        }
-        return false;
-    }
-
-	void searchHelper(std::vector<float> pointToCheck, Node* node, uint depth, float distanceTol, std::vector<int>& ids)
-    {
-        if (node != NULL) {
-            if (isInBox(pointToCheck, node->point, distanceTol)) {
-                if (isInDistanceTol(pointToCheck, node->point, distanceTol)) {
-                    // add point within distanceTol to the return list
-                    ids.push_back(node->id);
-                }
-            }
-
-            // next node depending on if the boxed square crosses over the divided x or y region
-            if ((pointToCheck[depth % 2] - distanceTol) < node->point[depth % 2]) {
-                searchHelper(pointToCheck, node->left, depth+1, distanceTol, ids);
-            }
-            if ((pointToCheck[depth % 2] + distanceTol) >= node->point[depth % 2]) {
-                searchHelper(pointToCheck, node->right, depth+1, distanceTol, ids);
-            }
-        }
+    float EuclideanDistance(std::vector<float> node, std::vector<float> pointToCheck)
+	{
+		float sum_squared = 0;
+		for (size_t i = 0; i < node.size(); i++)
+		{
+			sum_squared += ((node[i] - pointToCheck[i]) * (node[i] - pointToCheck[i]));
+		}
+		double distance = sqrt(sum_squared);
+		return distance;
 	}
+
+
+    std::vector<int> searchHelper(std::vector<float> pointToCheck, Node *&node, uint depth, float distanceTol, std::vector<int> &ids)
+    {
+        uint splitAxis = depth % 3;
+
+        if (node != NULL) 
+        {
+            if ((node->point[0] <= (pointToCheck[0] + distanceTol) && node->point[0] >= (pointToCheck[0] - distanceTol) &&
+				node->point[1] <= (pointToCheck[1] + distanceTol) && node->point[1] >= (pointToCheck[1] - distanceTol) &&
+				node->point[2] <= (pointToCheck[2] + distanceTol) && node->point[2] >= (pointToCheck[2] - distanceTol))) 
+            {
+				
+                float distance = EuclideanDistance(node->point, pointToCheck);
+
+				// Add node's ID only if node's distance is within distance tolerance
+				if (distance <= distanceTol)
+				{
+					ids.push_back(node->id);
+				}
+            }
+            // Intersection of the bounding box with the split line
+			if (node->point[splitAxis] <= (pointToCheck[splitAxis] + distanceTol) && node->point[splitAxis] >= (pointToCheck[splitAxis] - distanceTol))
+			{
+				searchHelper(pointToCheck, node->left, distanceTol, depth + 1, ids);
+				searchHelper(pointToCheck, node->right, distanceTol, depth + 1, ids);
+			}
+			// Recursive 2a - if node's cutting axis is NOT intersecting bounding box - min bound
+			else if (node->point[splitAxis] < (pointToCheck[splitAxis] - distanceTol))
+			{
+				searchHelper(pointToCheck, node->right, distanceTol, depth + 1, ids);
+			}
+			// Recursive 2b - if node's cutting axis is NOT intersecting bounding box - max bound
+			else
+			{
+				searchHelper(pointToCheck, node->left, distanceTol, depth + 1, ids);
+			}
+	    }
+
+        return ids;
+    }
+
+
+	// // check if the node point is within a boxed square which is 2 times the tolerance
+    // bool isInBox(std::vector<float> pointToCheck, std::vector<float> treePoint, float distanceTol)
+    // {
+    //     if ((treePoint[0] < (pointToCheck[0] - distanceTol)) || (treePoint[0] > (pointToCheck[0] + distanceTol)) || (treePoint[1] < (pointToCheck[1] - distanceTol)) || (treePoint[1] > (pointToCheck[1] + distanceTol))) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+
+    // // calculate if the distance between node point and point to check point is within tolerance
+    // bool isInDistanceTol(std::vector<float> pointToCheck, std::vector<float> treePoint, float distanceTol)
+    // {
+    //     float d = sqrtf(pow((pointToCheck[0] - treePoint[0]), 2.0) + pow((pointToCheck[1] - treePoint[1]), 2.0));
+
+    //     if (d < distanceTol) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> pointToCheck, float distanceTol)
@@ -106,7 +141,3 @@ struct KdTree
 	
 
 };
-
-
-
-
